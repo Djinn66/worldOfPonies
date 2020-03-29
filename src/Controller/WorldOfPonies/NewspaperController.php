a22740893889251fa80cd05852bcd36213d0d4b5
Laurent <laurent.cuadras@etu.univ-amu.fr>
2020-03-29 19:18:54 +0200

    Menu

--- src/Controller/WorldOfPonies/NewspaperController.php
+++ src/Controller/WorldOfPonies/NewspaperController.php
@@ -14,7 +14,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 /**
  * @Route("/worldofponies/newspaper")
- * @Security("is_granted('ROLE_PROGRAMMER') or is_granted('ROLE_SUPERUSER') or is_granted('ROLE_NEWSREADER')")
+ * @Security("is_granted('ROLE_PROGRAMMER') or is_granted('ROLE_SUPERUSER') or is_granted('ROLE_EDITOR') or is_granted('ROLE_NEWSREADER')")
  */
 class NewspaperController extends AbstractController
 {
